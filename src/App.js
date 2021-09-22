049b3c0743219960f8d6d31c044d123ecbe0ba5a
candra kriswinarto <ckriswinarto@gmail.com>
2021-09-23 07:42:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import {
+  Animator,
+  batch,
+  Fade,
+  FadeIn,
+  Move,
+  MoveIn,
+  MoveOut,
+  ScrollContainer,
+  ScrollPage,
+  Sticky,
+  StickyIn,
+  ZoomIn
+} from 'react-scroll-motion';
 import './App.css';
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Sticky(), Move())
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScrollContainer>
+      <ScrollPage page={0}>
+        <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+          <h2>Let me show you scroll animation 😀</h2>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={1}>
+        <Animator animation={ZoomInScrollOut}>
+          <h2>I'm, FadeUpScrollOut ✨</h2>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={2}>
+        <Animator animation={FadeUp}>
+          <h2>I'm FadeUp ☁️</h2>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={3}>
+        <div className="section-3">
+          <h2>
+            <Animator animation={MoveIn(-1000, 0)}>Hello guys👋🏻</Animator>
+            <Animator animation={MoveIn(1000, 0)}>Nice to meet you 👱🏻‍♂️</Animator>
+            <Animator animation={MoveOut(1000, 0)}>Good Bye ✋🏻</Animator>
+            <Animator animation={MoveOut(-1000, 0)}>See you 🧡</Animator>
+          </h2>
+        </div>
+      </ScrollPage>
+
+      <ScrollPage page={4}>
+        <Animator animation={batch(Fade(), Sticky())}>
+          <h2>Done ✅</h2>
+        </Animator>
+      </ScrollPage>
+    </ScrollContainer>
   );
 }
 
